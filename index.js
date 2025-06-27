d476b0e348074783439183e1f0530e7eca88d52d
G1ANO <iwmuthiani@yahoo.com>
2025-06-28 01:12:36 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+window.newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ian Muthiani is the champion';
+
+document.body.appendChild(newHeader);
